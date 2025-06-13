31cc75f4150c7beed831833f95f1ae7c0b24a217
iarlamatos07 <iarlamatos07@gmail.com>
2025-06-13 00:13:17 -0300

    teste

--- Exercicios/src/Exe10.java
+++ Exercicios/src/Exe10.java
@@ -14,57 +14,49 @@ public class Exe10 {
         int plano = sc.nextInt();
         String nomePlano = "";
 
-        if (plano == 1){
+        if (plano == 1) {
             nomePlano = "Convênio";
-           System.out.println("Seu plano: Convênio");
-        }
-        else if (plano == 2){
+            System.out.println("Seu plano: Convênio");
+        } else if (plano == 2) {
             nomePlano = "Particular";
-           System.out.println("Seu plano: Particular");
-        }
-        else {
+            System.out.println("Seu plano: Particular");
+        } else {
             System.out.println("Valor de plano inválido");
-
             System.exit(0);
         }
 
-        System.out.println("\nInforme a especialidade desejada:\n1- Clinica Geral\n2- Pediatria\n3- Cardiologia\n4- Ortopedia\n");
+        System.out.println("\nInforme a especialidade desejada:\n1- Clínica Geral\n2- Pediatria\n3- Cardiologia\n4- Ortopedia\n");
         int especialidade = sc.nextInt();
         String nomeEspecialidade = "";
 
         switch (especialidade) {
-
             case 1: System.out.println("Especialidade: Clínica Geral"); nomeEspecialidade = "Clínica Geral"; break;
             case 2: System.out.println("Especialidade: Pediatria"); nomeEspecialidade = "Pediatria"; break;
             case 3: System.out.println("Especialidade: Cardiologia"); nomeEspecialidade = "Cardiologia"; break;
             case 4: System.out.println("Especialidade: Ortopedia"); nomeEspecialidade = "Ortopedia"; break;
-            case 5: System.out.println("Valor para Especialidade Inválido!"); System.exit(0); break;
-
+            default: System.out.println("Valor para especialidade inválido!"); System.exit(0); break;
         }
 
-        System.out.println("\nIdade informada: " + idade +" anos");
+        System.out.println("\nIdade informada: " + idade + " anos");
         System.out.println("Plano médico escolhido: " + nomePlano);
         System.out.println("Especialidade: " + nomeEspecialidade);
 
-        if (idade > 65){
+        if (idade > 65) {
             System.out.println("\nIDOSO: Atendimento Prioritário!");
-        }
-        else if (idade <= 12) {
+        } else if (idade <= 12) {
             System.out.println("\nCRIANÇA: Atendimento Prioritário!");
-        }
-        else {
+        } else {
             System.out.println("\nAtendimento NORMAL!");
         }
 
         if (nomePlano.equals("Convênio") && idade > 60) {
-            System.out.println("Isento de taxa de consulta");
-        } else if (nomePlano.equals("Convênio") && idade <= 60) {
-            System.out.println("Taxa reduzida: R$40,00");
+            System.out.println("Isento de taxa de consulta!");
+        } else if (nomePlano.equals("Convênio") && idade >= 0 && idade <= 60) {
+            System.out.println("Taxa reduzida: R$ 40,00.");
         } else if (nomePlano.equals("Particular")) {
-            System.out.println("Taxa integral: R$100,00");
+            System.out.println("Taxa integral: R$ 100,00.");
         }
 
-        // Gerador de senha aleatória
         int senhaAleatoria = random.nextInt(90) + 1;
         System.out.println("\nSenha de atendimento: " + senhaAleatoria);
 
